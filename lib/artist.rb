1755a8bbae3a197b3f18bec467b966eeecf8ef1a
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 05:21:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,11 +23,8 @@ class Artist
   end
 
   
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  def add_song(song)
+
+def add_song(song)
     @songs << song
     song.artist = self
   end

dea9c7e1c1bbfe1dc2a51cac3435e636c972dfb3
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 05:07:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,13 +2,15 @@ require 'pry'
 
 class Artist
   
+  attr_accessor :name
+  attr_reader :songs
+  
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
   
-  attr_accessor :name
-  attr_reader :songs
+  
 
   @@artists = []
   

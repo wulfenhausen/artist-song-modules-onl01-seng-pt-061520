5b327bc6184dd3e8ab4506dabcac14b8f1a15532
William Han Findiesen <william.findiesen@gmail.com>
2020-07-28 05:13:42 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
   
   module ClassMethods
-    def self.count
+    def count
       self.all.count
     end
     
-    def self.reset_all
+    def reset_all
       self.all.clear
     end
   end
